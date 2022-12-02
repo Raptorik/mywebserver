045620536a19fb04f8445024f07e2aee1fd32548
Raptorik <118775064+Raptorik@users.noreply.github.com>
2022-12-02 18:00:52 +0200

    Update mywebserver.go

--- mywebserver.go
+++ mywebserver.go
@@ -3,26 +3,17 @@ package mywebserver
 import (
 	"context"
 	"errors"
+	"github.com/gorilla/mux"
 	"log"
+	"mvc/app/controllers"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
-type ControllersSet interface {
-	Controllers() []Controller
-}
-
-type Controller interface {
-	ArtistController() string
-	ExhibitionController() string
-}
-
-func StartServer(cs Controller) {
+func StartServer() {
 	ctx := context.Background()
 	router := mux.NewRouter()
 	srv := &http.Server{
@@ -33,20 +24,33 @@ func StartServer(cs Controller) {
 		ReadHeaderTimeout: time.Millisecond * 200,
 		Handler:           router,
 	}
+	artistC := &controllers.ArtistController{}
+	artC := &controllers.ArtController{}
+	galleryC := &controllers.ExhibitionController{}
 
-	router.HandleFunc(cs.ArtistController(), func(http.ResponseWriter, *http.Request) {
-		cs.InviteArtist(`Artist invited`)
-	})
-	router.HandleFunc(cs.ExhibitionController(), func(http.ResponseWriter, *http.Request) {
-		cs.OrganizeExhibition(`Exhibition opened`)
-	})
+	// CREATE AN ART
+	// localhost:8080/createart/blackCat
+	router.HandleFunc("/createart/{art}", artC.ArtCreation)
 
-	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		_, _ = rw.Write([]byte("Hello!"))
-	})
+	// CREATE AN ARTIST
+	// localhost:8080/createartist/Fillip
+	router.HandleFunc("/createartist/{artist}", artistC.Registration)
 
-	http.Handle("/", router)
+	// CREATE EXHIBITION
+	// localhost:8080/creategallery/Tokio
+	router.HandleFunc("/createexhibition/{exhibition}", galleryC.ExhibitionCreation)
 
+	//ASSIGN AN ART TO THE ARTIST (BY NAME)
+	// localhost:8080/artist/assign/Fillip/blackCat
+	router.HandleFunc("/artist/assign/{artist}/{art}", artC.AssignArt)
+
+	//REGISTRATION AN ARTIST ON THE EXHIBITION
+	// localhost:8080/artist/register/Fillip/Tokio
+	router.HandleFunc("/artist/register/{artist}/{exhibition}", artistC.ArtistRegistration)
+
+	// DELETE AN ARTIST FROM THE EXHIBITION
+	// localhost:8080/artist/delete/Fillip/Tokio
+	router.HandleFunc("/artist/delete/{artist}/{gallery}", galleryC.KickArtistOffExhibition)
 	go func() {
 		log.Println(`Web Server started`)
 		err := srv.ListenAndServe()
