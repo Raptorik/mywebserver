f694e4899ca4aa2467d7621678ad7a43d28e2142
Raptorik <118775064+Raptorik@users.noreply.github.com>
2022-12-06 10:14:41 +0200

    Update mywebserver.go

--- mywebserver.go
+++ mywebserver.go
@@ -1,19 +1,23 @@
-package mywebserver
+package webservergo
 
 import (
 	"context"
 	"errors"
-	"github.com/gorilla/mux"
 	"log"
-	"mvc/app/controllers"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+	"github.com/gorilla/mux"
 )
 
-func StartServer() {
+type Controller interface {
+	RegisterRouter(*mux.Router)
+	RegisterActions()
+}
+
+func StartServer(controllers ...Controller) {
 	ctx := context.Background()
 	router := mux.NewRouter()
 	srv := &http.Server{
@@ -24,33 +28,12 @@ func StartServer() {
 		ReadHeaderTimeout: time.Millisecond * 200,
 		Handler:           router,
 	}
-	artistC := &controllers.ArtistController{}
-	artC := &controllers.ArtController{}
-	galleryC := &controllers.ExhibitionController{}
 
-	// CREATE AN ART
-	// localhost:8080/createart/blackCat
-	router.HandleFunc("/createart/{art}", artC.ArtCreation)
+	for _, concontroller := range controllers {
+		concontroller.RegisterRouter(router)
+		concontroller.RegisterActions()
+	}
 
-	// CREATE AN ARTIST
-	// localhost:8080/createartist/Fillip
-	router.HandleFunc("/createartist/{artist}", artistC.Registration)
-
-	// CREATE EXHIBITION
-	// localhost:8080/creategallery/Tokio
-	router.HandleFunc("/createexhibition/{exhibition}", galleryC.ExhibitionCreation)
-
-	//ASSIGN AN ART TO THE ARTIST (BY NAME)
-	// localhost:8080/artist/assign/Fillip/blackCat
-	router.HandleFunc("/artist/assign/{artist}/{art}", artC.AssignArt)
-
-	//REGISTRATION AN ARTIST ON THE EXHIBITION
-	// localhost:8080/artist/register/Fillip/Tokio
-	router.HandleFunc("/artist/register/{artist}/{exhibition}", artistC.ArtistRegistration)
-
-	// DELETE AN ARTIST FROM THE EXHIBITION
-	// localhost:8080/artist/delete/Fillip/Tokio
-	router.HandleFunc("/artist/delete/{artist}/{gallery}", galleryC.KickArtistOffExhibition)
 	go func() {
 		log.Println(`Web Server started`)
 		err := srv.ListenAndServe()
